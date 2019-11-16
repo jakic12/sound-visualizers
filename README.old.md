e2f187e856970a4c7f34e4cc9e993acbaeaefa6f
jakob <drusany.jakob@gmail.com>
2019-11-17 00:29:06 +0100

    Added basic file structure - modified create-react-app

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# sound-visualizers
+Sound visualizers
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles/app.scss";
+
+function App() {
+  return <div className="app"></div>;
+}
+
+export default App;
